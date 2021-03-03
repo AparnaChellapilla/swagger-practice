5ef86539655c79a6c0f850c33544bd549ac1d479
AC <75492066+AparnaChellapilla@users.noreply.github.com>
2021-03-03 13:19:13 -0600

    config file modified with @EnableSwagger2 annotation

--- src/main/java/com/tts/swaggerpractice/config/SwaggerConfig.java
+++ src/main/java/com/tts/swaggerpractice/config/SwaggerConfig.java
@@ -12,8 +12,10 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.DocExpansion;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
         @Bean
